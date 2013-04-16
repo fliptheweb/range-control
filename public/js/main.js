112c3dcbc90030a51d337519776193aacfc3a2dc
Artur Kornakov <itisshortemailaddress@gmail.com>
2013-04-16 22:41:00 +1100

    Begin build range table from data of cells

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,67 @@
 //@ sourceMappingURL=main.map
 (function() {
-  var RangeControl;
+  var RangeControl, RangeTable;
 
   RangeControl = (function() {
+    RangeControl.rangeTable;
+
     function RangeControl(el) {
       this.el = el;
-      console.log(this.el);
+      this.buildRangeTable();
     }
 
+    RangeControl.prototype.buildRangeTable = function() {
+      return this.rangeTable = new RangeTable(this.el.find(".range-control__range table"));
+    };
+
     return RangeControl;
+
+  })();
+
+  RangeTable = (function() {
+    RangeTable.data;
+
+    function RangeTable(el) {
+      this.el = el;
+      this.rangeCells = this.el.find("td");
+      this.data = [];
+      this.buildDataFromCells();
+      this.bindHoverToCells();
+    }
+
+    RangeTable.prototype.buildDataFromCells = function() {
+      var x,
+        _this = this;
+
+      this.rangeCells.each(function(i, rangeCell) {
+        rangeCell = $(rangeCell);
+        return _this.data.push({
+          volume: rangeCell.data("volume"),
+          rate: rangeCell.data("rate")
+        });
+      });
+      this.maxVolume = Math.max.apply(null, (function() {
+        var _i, _len, _ref, _results;
+
+        _ref = this.data;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          x = _ref[_i];
+          _results.push(x.volume);
+        }
+        return _results;
+      }).call(this));
+      return console.log(this.maxVolume);
+    };
+
+    RangeTable.prototype.bindHoverToCells = function() {
+      return this.rangeCells.on("mouseover", function() {
+        $(this).append($("<div />").text($(this).data("volume")));
+        return console.log();
+      });
+    };
+
+    return RangeTable;
 
   })();
 
--- src/coffee/rangeControl.coffee
+++ src/coffee/rangeControl.coffee
@@ -1,6 +1,39 @@
 class RangeControl
+  @rangeTable
+
   constructor: (@el) ->
-    console.log @el
+    @buildRangeTable()
+
+  buildRangeTable: ->
+    @rangeTable = new RangeTable(@el.find(".range-control__range table"))
+
+#      rangeCell.on "mouseover", ->
+#        console.log($(@).data("rate"))
+
+class RangeTable
+  @data
+
+  constructor: (@el) ->
+    @rangeCells = @el.find("td")
+    @data = []
+    @buildDataFromCells()
+    @bindHoverToCells()
+
+  buildDataFromCells: ->
+    @rangeCells.each (i, rangeCell)=>
+      rangeCell = $(rangeCell)
+      @data.push
+        volume: rangeCell.data "volume"
+        rate:   rangeCell.data "rate"
+
+    @maxVolume = Math.max.apply null, (x.volume for x in @data)
+    console.log @maxVolume
+
+  bindHoverToCells: ->
+    @rangeCells.on "mouseover", ->
+      $(@).append($("<div />").text($(@).data("volume")))
+      console.log()
+
 
 
 new RangeControl($(".range-control"))
